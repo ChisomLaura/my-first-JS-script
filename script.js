4c84e92cefe091c8c4c8aa3a3a1b40d268dd250c
Chisom Laura <102640478+ChisomLaura@users.noreply.github.com>
2022-06-07 21:02:54 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Write your JavaScript below this line
+var name = "Laura";
+var favouriteSnack = "cookies";
+console.log(
+  "Hi my name is " +
+    name +
+    " and I wish I had " +
+    favouriteSnack +
+    " right now."
+);
+var numberOfHorses = 22;
+console.log(numberOfHorses);
+
+var favouriteFlowers = "pink roses";
+console.log("My favourite flowers are " + favouriteFlowers);
+
+var cityOfBirth = "Port-Harcourt";
+var birthday = "March 4,2000";
+console.log("I was born in " + cityOfBirth + " on the " + birthday);
